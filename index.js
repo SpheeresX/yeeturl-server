774c738d934c15c55fae9b101e8d008ccdde8681
SpheeresX <76824533+SpheeresX@users.noreply.github.com>
2021-04-13 15:19:44 +0200

    I HATE REPL.IT

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // The server has been moved to server.js
 if (!process.env.YURL_TEST) require('dotenv').config();
-require('server').run(process.env.PORT || 3000, process.env.MONGODB_URL, false);
+require('./server').run(process.env.PORT || 3000, process.env.MONGODB_URL, false);
